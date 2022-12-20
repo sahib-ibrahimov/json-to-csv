91b5ad8a2fb43b0a6ac1ab1341c34c79c8b5181d
sahib-ibrahimov <sahib.ibrahimov1998@gmail.com>
2022-12-20 19:11:23 +0400

    init

---
+++ main.js
@@ -0,0 +1,48 @@
+const data = new Array();
+const div = $('.data');
+
+$('form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  const newData = FormToJSON(e.target);
+  if( data.find(item => item.name === newData.name) ) {
+    alert('tekrar');
+  } else {
+    data.push( newData );
+    div.innerText = dataToText(data);
+    e.target.reset();
+  }
+});
+
+$('.save').addEventListener('click', () => {
+  downloadAsFile( dataToText(data) )
+});
+
+$('.print').addEventListener('click', () => {
+  const wnd = window.open('', 'window', 'width=300,height=300');
+  wnd.document.body.append( div.cloneNode(true) );
+  wnd.print();
+  wnd.close();
+});
+
+function downloadAsFile(data, fileName = '') {
+  let a = document.createElement("a");
+  let file = new Blob([data], {type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'});
+  a.href = URL.createObjectURL(file);
+  a.download = fileName;
+  a.click();
+}
+
+function dataToText(data) {
+  const temp = new Array();
+  const keys = Object.keys(data[0]);
+  const t = [];
+  for(const key of keys) t.push(key);
+  temp.push( t.join('\t') );
+  data.forEach(item => {
+    const t = [];
+    for(const key of keys)
+      t.push(item[key]);
+    temp.push( t.join('\t') );
+  });
+  return temp.join('\n');
+}
